82a47920be1b840a293c7e37a11bd2cb30ef84cc
Romain Vaur <rvaur@hotmail.com>
2024-09-16 15:38:14 +0200

    DB - setup local database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.la4he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
